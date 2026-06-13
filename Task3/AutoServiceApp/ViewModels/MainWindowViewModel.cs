c6c40ab9bd34fdb17ba12a9fcce138612848f385
narekii <bimarahmat20@gmail.com>
2026-06-14 02:27:53 +0700

    defects: speculative generality method: remove dead code view models aren't used here since the view are directly communicating with model layer, so it's safe to assume that this project is not using mvvm and im deleting the view models

--- Task3/AutoServiceApp/ViewModels/MainWindowViewModel.cs
+++
@@ -1,9 +0,0 @@
-using AutoServiceApp.Services;
-
-namespace AutoServiceApp.ViewModels;
-
-public class MainWindowViewModel
-{
-    public AutoServiceManager Manager { get; set; } = new();
-    public string Title { get; set; } = "Auto Service";
-}
